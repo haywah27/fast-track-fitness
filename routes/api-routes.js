b02a71668fb69f72c7a2b4ab7f54c56fb5398a5c
haywah27 <hayleywahlroos15@gmail.com>
2021-03-18 14:15:37 -0700

    initializing api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,21 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 // create workout
+router.post("api/workouts", (req, res) => {
+
+});
+
 // read workout
+router.get("api/workouts", (req, res) => {
+
+});
+
 // update workout
+router.put("api/workouts:id", (req, res) => {
+
+});
+
 // delete workout
+router.delete("api/workouts", (req, res) => {
+
+});
