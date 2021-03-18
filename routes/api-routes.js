f1f8250156197af3981599a77ba947f535c11515
haywah27 <hayleywahlroos15@gmail.com>
2021-03-18 16:32:16 -0700

    reworking api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,46 +1,16 @@
+const db = require("../models");
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+
 
 // create workout
-router.post("/workouts", (req, res) => {
-    Workout.create({})
-      .then(dbWorkout => {
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).then((dbWorkout => {
         res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-});
-
-// read workout
-router.get("/workouts", (req, res) => {
-    Workout.find()
-      .then(dbWorkouts => {
-        res.json(dbWorkouts);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-});
-
-// update workout
-router.put("/workouts:id", (req, res) => {
-    Workout.findByIdAndUpdate(
-        { _id: req.params.id },
-        { $push: { exercises: req.body } },
-        { new: true, runValidators: true }
-    )
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
+    })).catch(err => {
         res.json(err);
     });
 });
 
-// // delete workout
-// router.delete("/workouts", (req, res) => {
 
-// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,16 +13,16 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/populate', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
-app.use('/', require("./routes/html-routes.js"));
-app.use('/api', require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 mongoose.connection.on('error', (err) =>
   console.log(`error in mongoose conneciton: ${err.message}`)
 );
 
 mongoose.connection.once('open', () => {
-  console.log('mongoose connected!');
+  console.log('mongoose connected');
   app.listen(PORT, (err) => console.log(`App is running at: http://localhost/${PORT}`));
 });
