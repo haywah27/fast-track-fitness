129e820697e12a8cf20e0e7195f70fcba797e54d
haywah27 <hayleywahlroos15@gmail.com>
2021-03-18 13:56:23 -0700

    creating schema

--- models/workout.js
+++ models/workout.js
@@ -1,8 +1,51 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
-const workoutSchema = new Schema()
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    trim: true,
+    default: Date.now,
+    required: true
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        validate: [({ length }) => length <= 50, 'Too many characters, please enter condensed exercise.'],
+        required: true
+      },
+      name: {
+        type: String,
+        trim: true,
+        validate: [({ length }) => length <= 50, 'Too many characters, please enter condensed name.'],
+        required: true
+      },
+      duration: {
+        type: Number,
+        trim: true,
+        required: true
+      },
+      weight: {
+        type: Number,
+        trim: true
+      },
+      reps: {
+        type: Number,
+        trim: true
+      },
+      sets: {
+        type: Number,
+        trim: true
+      },
+      distance: {
+        type: Number,
+        trim: true
+      }
+    }
+  ]
+});
   
   
 const Workout = mongoose.model("Workout", workoutSchema);
