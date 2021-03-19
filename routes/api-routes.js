49c5bb741b0299a4a561180bc322342f5df445fb
haywah27 <hayleywahlroos15@gmail.com>
2021-03-19 12:58:19 -0700

    route db for graphs using aggregate funcitonality

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -37,13 +37,12 @@ router.put("/api/workouts/:id", (req, res) => {
 
 
 // route for workout dashboard
-router.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
-    .then(response => {
-        res.json(response);
-    }).catch(err => {
-        res.json(err);
-    });
+router.get("/api/workouts/range", async (req, res) => {
+    res.json(
+      await db.Workout.aggregate([
+        { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+      ])
+    );
 });
 
 
